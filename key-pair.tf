372d73f5cc89f9de47a46f25a433a19559de7fbe
Lamyanba123 <destinytasteme@gmail.com>
2022-09-26 01:01:46 +0530

    modified files

--- key-pair.tf
+++ key-pair.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
-
 resource "aws_key_pair" "test_key" {
   key_name   = "test-key"
   public_key = file("${path.module}/id_rsa.pub")
