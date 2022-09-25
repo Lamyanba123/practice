819502f626ca9801751176d6aae911f3edddb469
Lamyanba123 <destinytasteme@gmail.com>
2022-09-26 01:04:29 +0530

    keyname passed as variable

--- key-pair.tf
+++ key-pair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "test_key" {
-  key_name   = "test-key"
+  key_name   = local.key_name
   public_key = file("${path.module}/id_rsa.pub")
 }
 
--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
   zone_name   = "codomation.tk"
   record_name = "web.codomation.tk"
+  key_name ="test-key"
 }
 
